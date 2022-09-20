a21a4081eb86ece4c3eee168f82252540851799a
Lucas Biazin <lucasbiazin07@gmail.com>
2022-09-20 16:28:54 -0300

    implementando a classe cliente e seus métodos

---
+++ src/Cliente.java
@@ -0,0 +1,74 @@
+
+public class Cliente {
+
+	private int id;
+	private String nome;
+	private String cpf;
+	private String telefone;
+	private String rua;
+	private int numeroPredial;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+
+	// Construtor
+
+	public Cliente(int id, String nome, String cpf, String telefone, String rua, int numeroPredial, String complemento,
+			String bairro, String cidade, String estado) {
+
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.rua = rua;
+		this.numeroPredial = numeroPredial;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+
+	// Getters
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public int getNumeroPredial() {
+		return numeroPredial;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+}
---
+++ src/MainCliente.java
@@ -0,0 +1,54 @@
+
+public class MainCliente {
+
+	public static void main(String[] args) {
+
+		Cliente Ana = new Cliente(1, "Ana", "111.111.111-11", "(44) 11111-1111", "Rua da praia", 77, "Casa",
+				"Porto Belo", "Florianopólis", "Santa Catarina");
+
+		Cliente Eli = new Cliente(2, "Eli", "222.222.222-22", "(44) 22222-2222", "Rua da Independência", 07, "Casa",
+				"Jardim Brasil", "Brasilia", "Distrito Federal");
+
+		Cliente Gabi = new Cliente(3, "Gabi", "333.333.333-33", "(44)33333-3333", "Avenida Tricolor", 1930, "Casa",
+				"Morumbi", "São Paulo", "São Paulo");
+
+		// Imprimir resultados
+
+		System.out.println(" ID: " + Ana.getId());
+		System.out.println(" Nome: " + Ana.getNome());
+		System.out.println(" CPF: " + Ana.getCpf());
+		System.out.println(" Telefone: " + Ana.getTelefone());
+		System.out.println(" Rua: " + Ana.getRua());
+		System.out.println(" Número do endereço: " + Ana.getNumeroPredial());
+		System.out.println(" Complemento: " + Ana.getComplemento());
+		System.out.println(" Bairro: " + Ana.getBairro());
+		System.out.println(" Cidade: " + Ana.getCidade());
+		System.out.println(" Estado: " + Ana.getEstado());
+		System.out.println(" ------------------------");
+
+		System.out.println(" ID: " + Eli.getId());
+		System.out.println(" Nome: " + Eli.getNome());
+		System.out.println(" CPF: " + Eli.getCpf());
+		System.out.println(" Telefone: " + Eli.getTelefone());
+		System.out.println(" Rua: " + Eli.getRua());
+		System.out.println(" Número do endereço: " + Eli.getNumeroPredial());
+		System.out.println(" Complemento: " + Ana.getComplemento());
+		System.out.println(" Bairro: " + Eli.getBairro());
+		System.out.println(" Cidade: " + Eli.getCidade());
+		System.out.println(" Estado: " + Eli.getEstado());
+		System.out.println(" ------------------------");
+
+		System.out.println(" ID: " + Gabi.getId());
+		System.out.println(" Nome: " + Gabi.getNome());
+		System.out.println(" CPF: " + Gabi.getCpf());
+		System.out.println(" Telefone: " + Gabi.getTelefone());
+		System.out.println(" Rua: " + Gabi.getRua());
+		System.out.println(" Número do endereço: " + Gabi.getNumeroPredial());
+		System.out.println(" Complemento: " + Gabi.getComplemento());
+		System.out.println(" Bairro: " + Gabi.getBairro());
+		System.out.println(" Cidade: " + Gabi.getCidade());
+		System.out.println(" Estado: " + Gabi.getEstado());
+
+	}
+
+}
